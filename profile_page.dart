94f22de4d9c9068bb370313abdd852bd8c16283e
InkaraT-SE18 <157287894+InkaraT-SE18@users.noreply.github.com>
2025-05-11 01:17:38 +0500

    Update profile_page.dart

--- profile_page.dart
+++ profile_page.dart
@@ -1,45 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../auth/auth_service.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      return const Center(child: Text("Вы не вошли в аккаунт"));
+    }
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-    
             const CircleAvatar(
               radius: 50,
-              backgroundImage: AssetImage('assets/hotels/hotel1.jpg'), 
+              backgroundImage: AssetImage('assets/hotels/hotel1.jpg'),
             ),
             const SizedBox(height: 16),
-
-            
-            const Text(
-              'Айгерим О.',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            Text(
+              user.displayName ?? 'Без имени',
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
-
             const SizedBox(height: 8),
-
-           
-            const Text(
-              'aigerim@gmail.com',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Text(
+              user.email ?? 'Нет email',
+              style: const TextStyle(fontSize: 16, color: Colors.grey),
             ),
-
             const SizedBox(height: 24),
-
-            
             ListTile(
               leading: const Icon(Icons.settings),
               title: const Text('Настройки'),
-              onTap: () {
-               
-              },
+              onTap: () {},
             ),
             ListTile(
               leading: const Icon(Icons.help_outline),
@@ -49,8 +46,8 @@ class ProfilePage extends StatelessWidget {
             ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Выход'),
-              onTap: () {
-                
+              onTap: () async {
+                await AuthService().signOut();
               },
             ),
           ],
