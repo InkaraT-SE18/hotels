deac921238bb27cf8f27ef28505197d48baa52d8
InkaraT-SE18 <157287894+InkaraT-SE18@users.noreply.github.com>
2025-05-11 01:14:22 +0500

    Update booking.dart

--- booking.dart
+++ booking.dart
@@ -1,57 +1,31 @@
 class Booking {
-  final int? id;
   final String hotelName;
   final String dateRange;
   final String roomType;
   final int guests;
- 
 
   Booking({
-    this.id,
     required this.hotelName,
     required this.dateRange,
     required this.roomType,
     required this.guests,
-    
   });
-
-  Booking copyWith({
-    int? id,
-    String? hotelName,
-    String? dateRange,
-    String? roomType,
-    int? guests,
-    
-  }) {
-    return Booking(
-      id: id ?? this.id,
-      hotelName: hotelName ?? this.hotelName,
-      dateRange: dateRange ?? this.dateRange,
-      roomType: roomType ?? this.roomType,
-      guests: guests ?? this.guests,
-      
-    );
-  }
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'hotelName': hotelName,
       'dateRange': dateRange,
       'roomType': roomType,
       'guests': guests,
-      
     };
   }
 
   factory Booking.fromMap(Map<String, dynamic> map) {
     return Booking(
-      id: map['id'],
       hotelName: map['hotelName'],
       dateRange: map['dateRange'],
       roomType: map['roomType'],
       guests: map['guests'],
-     
     );
   }
 }
